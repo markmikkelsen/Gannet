9a62d3652623377f09ba9beca2f997da5dc1ed9f
markmikkelsen <mark.mikkelsen@gmail.com>
2023-08-23 18:46:47 -0400

    Cosmetic updates

--- GannetMask_Philips.m
+++ GannetMask_Philips.m
@@ -14,31 +14,31 @@ if all(isstrprop(ext(end-3:end), 'upper'))
 else
     spar_ext = 'spar';
 end
-sparname = [fname(1:(end-4)) spar_ext];
-sparname = fopen(sparname,'r');
+sparname     = [fname(1:(end-4)) spar_ext];
+sparname     = fopen(sparname,'r');
 sparheadinfo = textscan(sparname, '%s');
 sparheadinfo = sparheadinfo{1};
 
-sparidx = find(ismember(sparheadinfo, 'ap_size')==1);
-MRS_struct.p.voxdim(ii,2) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'lr_size')==1);
-MRS_struct.p.voxdim(ii,1) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'cc_size')==1);
-MRS_struct.p.voxdim(ii,3) = str2double(sparheadinfo{sparidx+2});
+sparidx = find(ismember(sparheadinfo, 'ap_size') == 1);
+MRS_struct.p.voxdim(ii,2) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'lr_size') == 1);
+MRS_struct.p.voxdim(ii,1) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'cc_size') == 1);
+MRS_struct.p.voxdim(ii,3) = str2double(sparheadinfo{sparidx + 2});
 
-sparidx = find(ismember(sparheadinfo, 'ap_off_center')==1);
-MRS_struct.p.voxoff(ii,2) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'lr_off_center')==1);
-MRS_struct.p.voxoff(ii,1) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'cc_off_center')==1);
-MRS_struct.p.voxoff(ii,3) = str2double(sparheadinfo{sparidx+2});
+sparidx = find(ismember(sparheadinfo, 'ap_off_center') == 1);
+MRS_struct.p.voxoff(ii,2) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'lr_off_center') == 1);
+MRS_struct.p.voxoff(ii,1) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'cc_off_center') == 1);
+MRS_struct.p.voxoff(ii,3) = str2double(sparheadinfo{sparidx + 2});
 
-sparidx = find(ismember(sparheadinfo, 'ap_angulation')==1);
-MRS_struct.p.voxang(ii,2) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'lr_angulation')==1);
-MRS_struct.p.voxang(ii,1) = str2double(sparheadinfo{sparidx+2});
-sparidx = find(ismember(sparheadinfo, 'cc_angulation')==1);
-MRS_struct.p.voxang(ii,3) = str2double(sparheadinfo{sparidx+2});
+sparidx = find(ismember(sparheadinfo, 'ap_angulation') == 1);
+MRS_struct.p.voxang(ii,2) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'lr_angulation') == 1);
+MRS_struct.p.voxang(ii,1) = str2double(sparheadinfo{sparidx + 2});
+sparidx = find(ismember(sparheadinfo, 'cc_angulation') == 1);
+MRS_struct.p.voxang(ii,3) = str2double(sparheadinfo{sparidx + 2});
 
 V         = spm_vol(nii_file);
 [T1, XYZ] = spm_read_vols(V);
@@ -49,7 +49,7 @@ V         = spm_vol(nii_file);
 voxdim          = abs(voxdim)';
 halfpixshift    = -voxdim(1:3)/2;
 halfpixshift(3) = -halfpixshift(3);
-XYZ = XYZ + repmat(halfpixshift, [1 size(XYZ,2)]);
+XYZ             = XYZ + repmat(halfpixshift, [1 size(XYZ,2)]);
 
 % Get information from SPAR - change later to be read in
 ap_size = MRS_struct.p.voxdim(ii,2);
@@ -86,27 +86,26 @@ vox_ctr = ...
 
 % Make rotations on voxel
 rad = pi/180;
-initrot = zeros(3,3);
 
-xrot      = initrot;
+xrot      = zeros(3,3);
 xrot(1,1) = 1;
-xrot(2,2) = cos(lr_ang *rad);
-xrot(2,3) = -sin(lr_ang*rad);
-xrot(3,2) = sin(lr_ang*rad);
-xrot(3,3) = cos(lr_ang*rad);
+xrot(2,2) = cos(lr_ang * rad);
+xrot(2,3) = -sin(lr_ang * rad);
+xrot(3,2) = sin(lr_ang * rad);
+xrot(3,3) = cos(lr_ang * rad);
 
-yrot      = initrot;
-yrot(1,1) = cos(ap_ang*rad);
-yrot(1,3) = sin(ap_ang*rad);
+yrot      = zeros(3,3);
+yrot(1,1) = cos(ap_ang * rad);
+yrot(1,3) = sin(ap_ang * rad);
 yrot(2,2) = 1;
-yrot(3,1) = -sin(ap_ang*rad);
-yrot(3,3) = cos(ap_ang*rad);
+yrot(3,1) = -sin(ap_ang * rad);
+yrot(3,3) = cos(ap_ang * rad);
 
-zrot      = initrot;
-zrot(1,1) = cos(cc_ang*rad);
-zrot(1,2) = -sin(cc_ang*rad);
-zrot(2,1) = sin(cc_ang*rad);
-zrot(2,2) = cos(cc_ang*rad);
+zrot      = zeros(3,3);
+zrot(1,1) = cos(cc_ang * rad);
+zrot(1,2) = -sin(cc_ang * rad);
+zrot(2,1) = sin(cc_ang * rad);
+zrot(2,2) = cos(cc_ang * rad);
 zrot(3,3) = 1;
 
 % Rotate voxel
@@ -115,20 +114,20 @@ vox_rot = xrot * yrot * zrot * vox_ctr.';
 % Calculate corner coordinates relative to xyz origin
 vox_ctr_coor = [lr_off ap_off cc_off];
 vox_ctr_coor = repmat(vox_ctr_coor.', [1,8]);
-vox_corner = vox_rot + vox_ctr_coor;
+vox_corner   = vox_rot + vox_ctr_coor;
 
-mask = zeros(1,size(XYZ,2));
-sphere_radius = sqrt((lr_size/2)^2 + (ap_size/2)^2 + (cc_size/2)^2);
-distance2voxctr = sqrt(sum((XYZ - repmat([lr_off ap_off cc_off].', [1 size(XYZ,2)])).^2, 1));
-sphere_mask(distance2voxctr <= sphere_radius) = 1;
+mask          = zeros(1,size(XYZ,2));
+sphere_radius = sqrt((lr_size/2).^2 + (ap_size/2).^2 + (cc_size/2).^2);
+dist2voxctr   = sqrt(sum((XYZ - repmat([lr_off ap_off cc_off].', [1 size(XYZ,2)])).^2, 1));
+sphere_mask(dist2voxctr <= sphere_radius) = 1;
 
 mask(sphere_mask == 1) = 1;
 XYZ_sphere = XYZ(:,sphere_mask == 1);
 
-tri = delaunayn([vox_corner.'; [lr_off ap_off cc_off]]);
-tn = tsearchn([vox_corner.'; [lr_off ap_off cc_off]], tri, XYZ_sphere.');
+tri      = delaunayn([vox_corner.'; [lr_off ap_off cc_off]]);
+tn       = tsearchn([vox_corner.'; [lr_off ap_off cc_off]], tri, XYZ_sphere.');
 isinside = ~isnan(tn);
-mask(sphere_mask==1) = isinside;
+mask(sphere_mask == 1) = isinside;
 
 mask = reshape(mask, V.dim);
 
--- voxel2world_space.m
+++ voxel2world_space.m
@@ -4,28 +4,28 @@ function [img_t, img_c, img_s] = voxel2world_space(V, voxel_ctr)
 bb   = spm_get_bbox(V,'fv');
 dims = round(diff(bb)'+1);
 cent = voxel_ctr';
+M    = V.mat;
 
-M = V.mat;
 TM0 = [1 0 0 -bb(1,1)+1
        0 1 0 -bb(1,2)+1
        0 0 1 -cent(3)
        0 0 0 1];
-TM = inv(TM0*M);
-TD = dims([1 2]);
+TM  = inv(TM0*M);
+TD  = dims([1 2]);
 
 CM0 = [1 0 0 -bb(1,1)+1
        0 0 1 -bb(1,3)+1
        0 1 0 -cent(2)
        0 0 0 1];
-CM = inv(CM0*M);
-CD = dims([1 3]);
+CM  = inv(CM0*M);
+CD  = dims([1 3]);
 
 SM0 = [0 -1 0 +bb(2,2)+1
        0  0 1 -bb(1,3)+1
        1  0 0 -cent(1)
        0  0 0 1];
-SM = inv(SM0*M);
-SD = dims([2 3]);
+SM  = inv(SM0*M);
+SD  = dims([2 3]);
 
 img_t = spm_slice_vol(V, TM, TD, 2)';
 img_c = spm_slice_vol(V, CM, CD, 2)';

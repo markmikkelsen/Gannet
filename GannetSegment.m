92ec64c2aeb10b309167e7662e063860db9987d7
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2025-09-11 15:04:18 -0400

    Move seg8.mat deletion inside BIDS condition
    The deletion of the *_seg8.mat file is now performed only when BIDS processing is enabled, preventing unnecessary file removal outside of BIDS workflows.

--- GannetSegment.m
+++ GannetSegment.m
@@ -121,15 +121,16 @@ for kk = 1:length(vox)
         BG  = [T1dir filesep 'c6' T1name T1ext];
 
         % Forward deformation field
-        [struc_dir, struc_name, struc_ext]      = fileparts(MRS_struct.mask.(vox{kk}).T1image{ii});
+        [struc_dir, struc_name, struc_ext] = fileparts(MRS_struct.mask.(vox{kk}).T1image{ii});
         MRS_struct.mask.(vox{kk}).fwd_def{ii,:} = fullfile(struc_dir, ['y_' struc_name struc_ext]);
-        seg_mat = fullfile(struc_dir, [struc_name '_seg8.mat']);
-        if exist(seg_mat,'file')
-            delete(seg_mat);
-        end
-        
+
         % BIDSify
-        if MRS_struct.p.bids            
+        if MRS_struct.p.bids
+            seg_mat = fullfile(struc_dir, [struc_name '_seg8.mat']);
+            if exist(seg_mat, 'file')
+                delete(seg_mat);
+            end
+
             if ~files_segmented
                 movefile(GM, probseg_fname{1});
                 movefile(WM, probseg_fname{2});

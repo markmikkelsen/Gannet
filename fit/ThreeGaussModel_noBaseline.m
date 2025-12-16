fb9511af53740cc6ad7104c4a4e5a52e3b072957
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2025-12-16 18:13:44 -0500

    Add three-peak Gaussian and Lorentzian models
    Added ThreeGaussModel_noBaseline.m and ThreeLorentzModel_noBaseline.m for fitting three-peak Gaussian and Lorentzian models without baseline. Minor formatting update in GABAGlxModel_noBaseline.m.

---
+++ fit/ThreeGaussModel_noBaseline.m
@@ -0,0 +1,11 @@
+function F = ThreeGaussModel_noBaseline(x, freq)
+% Function for three-Gaussian model without a baseline
+
+% x(1-3)   = amplitudes
+% x(4-6)   = widths ( 1/(2*sigma^2) )
+% x(7)     = center freq of peak
+% x(8)     = J-coupling constant
+
+F = x(1) * exp(x(4) * (freq - x(7)).^2) + ...
+    x(2) * exp(x(5) * (freq - x(8)).^2) + ...
+    x(3) * exp(x(6) * (freq - x(9)).^2);
